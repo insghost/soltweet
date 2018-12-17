a4c1763345fbb25928e9e074a7c40d471de63e2e
Max Johansen <mjohansen@instructure.com>
2018-12-16 17:22:23 -0700

    css

--- soltweet-frontend/src/App.tsx
+++ soltweet-frontend/src/App.tsx
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from './components/container'
+import styled from '@emotion/styled'
+import { jsx } from '@emotion/core'
+
+const H1 = styled.h1`
+  text-align: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <div>
+          <H1>SolTweet</H1>
+        </div>
+      </Container>
     );
   }
 }
 
-export default App;
+export default App
