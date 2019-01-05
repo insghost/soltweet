a3086535c78434c1e52862fc9c674296735e86ab
Max Johansen <mjohansen@instructure.com>
2019-01-05 14:56:33 -0700

    Updates

---
+++ precache-manifest.ee72cca3c1a2e11e2f6c6a1d968ce561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2a0863b79b625c0ec8d",
+    "url": "/soltweet/static/css/main.9664b19d.chunk.css"
+  },
+  {
+    "revision": "e2a0863b79b625c0ec8d",
+    "url": "/soltweet/static/js/main.e2a0863b.chunk.js"
+  },
+  {
+    "revision": "71e769d163b5fb65a0ac",
+    "url": "/soltweet/static/js/1.71e769d1.chunk.js"
+  },
+  {
+    "revision": "4123c5df285accbf12c0",
+    "url": "/soltweet/static/js/runtime~main.4123c5df.js"
+  },
+  {
+    "revision": "071b3868b21f7d0852fdba72f10958f3",
+    "url": "/soltweet/index.html"
+  }
+];
